876f6525b3f924b73d07ddac240f310ab2dab56c
Belminh <belminhus@gmail.com>
2017-11-28 10:07:16 +0100

    28.nov

--- Dyrlege/kundeskjema.js
+++ Dyrlege/kundeskjema.js
@@ -9,28 +9,46 @@ function setup() {
         messagingSenderId: "96399653925"
     };
     firebase.initializeApp(config);
-
-    let divListe = document.getElementById("liste");
+    let spanKunde = document.getElementById("kundevelger");
     let divDyr = document.getElementById("dyr");
 
     let ref = firebase.database().ref("kunde");
 
-    ref.once("value").
-        then(function (snapshot) {
-            let kundene = snapshot.val();
-            if (kundene) {
-                let dropBox = makeDrop(kundene);
-                divListe.innerHTML = dropBox;
+    ref.once("value").then(function (snapshot) {
+        let kundene = snapshot.val();
+        if (kundene) {
+            let dropBox = makeDrop(kundene);
+            spanKunde.innerHTML = dropBox;
+
+            let drpKunde = document.getElementById("kundenr");
+            drpKunde.addEventListener("change", visDyr);
+        }
+    });
+
+    function visDyr(e) {
+        let valgt = +document.getElementById("kundenr").value;
+        let ref = firebase.database().ref("dyr").orderByChild("kundenr").equalTo(valgt);
+        ref.once("value").then(function (snapshot) {
+            let dyrene = snapshot.val();
+            if (dyrene) {
+                let dyrnr = Object.keys(dyrene);
+                let dyrliste = `<ol>` +
+                    dyrnr.map(e => `<li>${dyrene[e].navn} ${dyrene[e].art}</li>`).join("")
+                    + `</ol>`;
+                divDyr.innerHTML = dyrliste;
 
             }
         });
+    }
+
 
     function makeDrop(kunder) {
         let box = '<select id="kundenr">';
         let kundenr = Object.keys(kunder);
         let navn = kundenr.map(e =>
-            `<option values="${e}">${kunder[e].fornavn}</option`);
-        box += navn.join("") + "</select";
+            `<option value="${e}">${kunder[e].fornavn}</option>`);
+        box += navn.join("") + "</select>";
+        return box;
     }
 
 }
