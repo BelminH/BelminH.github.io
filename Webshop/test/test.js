7c548a5f5e49f447f8a7b96d2b2d2c53c1321027
Belmin Husanovic <BelminHusanovic@Belmins-MacBook-Pro.local>
2018-03-12 14:50:21 +0100

    12.mars 2018

--- Webshop/test/test.js
+++ Webshop/test/test.js
@@ -1,28 +1,103 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyCti2bGCwBe1KbxG_VpKp3_PUnOY78lgX0",
-    authDomain: "webchat-test-b7ef0.firebaseapp.com",
-    databaseURL: "https://webchat-test-b7ef0.firebaseio.com",
-    projectId: "webchat-test-b7ef0",
-    storageBucket: "webchat-test-b7ef0.appspot.com",
-    messagingSenderId: "237635251490"
-};
-firebase.initializeApp(config);
+function setup() {
 
-// When the user presses enter on the message input, write the message to firebase.
-$("#messageInput").keypress(function (e) {
-    if (e.keyCode == 13) {
-        var name = $("#nameInput").val();
-        var text = $("#messageInput").val();
-        messagesRef.push({ name: name, text: text });
-        $("#messageInput").val("");
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDk8WaEwZ89uXjqFj0wo2J0Nfmdi3idV3E",
+        authDomain: "webchat-266a7.firebaseapp.com",
+        databaseURL: "https://webchat-266a7.firebaseio.com",
+        projectId: "webchat-266a7",
+        storageBucket: "webchat-266a7.appspot.com",
+        messagingSenderId: "1006371844842"
+    };
+    firebase.initializeApp(config);
+    initApp();
+}
+
+function toggleSignIn() {
+    if (firebase.auth().currentUser) {
+        firebase.auth().signOut();
+    } else {
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+        if (email.length < 4) {
+            alert('Please enter an email address.');
+            return;
+        }
+        if (password.length < 4) {
+            alert('Please enter a password.');
+            return;
+        }
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode === 'auth/wrong-password') {
+                alert('Wrong password.');
+            } else {
+                alert(errorMessage);
+            }
+            console.log(error);
+            document.getElementById('sign-in').disabled = false;
+        });
     }
-});
+    document.getElementById('sign-in').disabled = true;
+}
 
-// Add a callback that is triggered for each chat message.
-messagesRef.limitToLast(10).on("child_added", function (snapshot) {
-    var message = snapshot.val();
-    $("<div/>").text(message.text).prepend($("<em/>")
-        .text(message.name + ": ")).appendTo($("#messagesDiv"));
-    $("#messagesDiv")[0].scrollTop = $("#messagesDiv")[0].scrollHeight;
-});
+function handleSignUp() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+    }
+    if (password.length < 4) {
+        alert('Please enter a password.');
+        return;
+    }
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
+    });
+}
+
+/**
+ * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+ *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+ *    out, and that is where we update the UI.
+ */
+function initApp() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            document.getElementById('login').classList.add("hidden");
+            document.getElementById('status').classList.remove("hidden");
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+
+            document.getElementById('signout').addEventListener('click', toggleSignIn, false);
+
+
+
+        } else {
+            // bruker må logge inn eller registrere seg
+            document.getElementById('login').classList.remove("hidden");
+            document.getElementById('sign-in').textContent = 'Sign in';
+            document.querySelector('#status').classList.add("hidden");
+        }
+
+        document.getElementById('sign-in').disabled = false;
+    });
+
+    document.getElementById('sign-in').addEventListener('click', toggleSignIn, false);
+    document.getElementById('sign-up').addEventListener('click', handleSignUp, false);
+}
