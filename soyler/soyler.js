3d7083c9eb14f8d9557cfae51559c184fe0618ba
Belmin Husanovic <BelminHusanovic@Belmins-MacBook-Pro.local>
2018-09-14 13:42:30 +0200

    14.sep 2018

--- soyler/soyler.js
+++ soyler/soyler.js
@@ -1,9 +1,48 @@
+// @ts-check
+
+class Person {
+    constructor(fornavn, etternavn, alder, vekt, hoyde) {
+        this.fornavn = fornavn;
+        this.etternavn = etternavn;
+        this.alder = alder;
+        this.vekt = vekt;
+        this.hoyde = hoyde;
+    }
+}
+
 function setup() {
     let divMain = document.getElementById("main");
+    let inpFornavn = document.getElementById("fornavn");
+    let inpEtternavn = document.getElementById("etternavn");
+    let inpAlder = document.getElementById("alder");
+    let inpHoyde = document.getElementById("hoyde");
+    let inpVekt = document.getElementById("vekt");
+    let btnLagre = document.getElementById("lagre");
 
-    for (i = 0; i < 10; i++) {
-        let s = lagSoyle(200 + Math.random() * 300);
-        setEtikkett(s, "ole johan OLSEN");
-        divMain.appendChild(s);
+    btnLagre.addEventListener("click", lagrePerson);
+
+    let personer = [];
+
+    function lagrePerson() {
+        let fornavn = inpFornavn.value;
+        let etternavn = inpEtternavn.value;
+        let vekt = inpVekt.valueAsNumber;
+        let alder = inpAlder.valueAsNumber;
+        let hoyde = inpHoyde.valueAsNumber;
+        let p = new Person(fornavn, etternavn, alder, vekt, hoyde);
+        personer.push(p);
+        lagSoylerForPersoner();
     }
+
+    function lagSoylerForPersoner() {
+        divMain.innerHTML = "";
+        for (let i = 0; i < personer.length; i++) {
+            let p = personer[i];
+            let s = lagSoyle(p.hoyde * 2);
+            setEtikkett(s, p.fornavn);
+            divMain.appendChild(s);
+        }
+    }
+
+
 }
